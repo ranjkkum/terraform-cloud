2fcfd804309ba75e9accd44fc88f9f6ef35ffa74
ranjan_k_kumar <ranjan.k.kumar@oracle.com>
2022-01-28 13:59:07 -0500

    test

--- bucket.tf
+++ bucket.tf
@@ -1,31 +1,3 @@
-variable "tenancy_ocid" {
+output {
+  value=max(1, 2, 3)
 }
-
-variable "user_ocid" {
-}
-
-variable "fingerprint" {
-}
-
-variable "private_key_path" {
-}
-
-variable "compartment_ocid" {
-}
-
-variable "region" {
-}
-
-data "oci_objectstorage_namespace" "test_namespace" {
-
-
-}
-
-resource "oci_objectstorage_bucket" "bucket2" {
-  compartment_id = var.compartment_ocid
-  namespace      = data.oci_objectstorage_namespace.test_namespace.namespace
-  name           = "buck2"
-  access_type    = "NoPublicAccess"
-  auto_tiering   = "Disabled"
-}
-
